5c789ed961c5cf1887fcd47152635ebd46f52cf9
Apurva-Sanpurkar <155803531+Apurva-Sanpurkar@users.noreply.github.com>
2026-02-26 18:14:18 +0530

    fixed apk issue

--- securecon-android/app/build.gradle.kts
+++ securecon-android/app/build.gradle.kts
@@ -1,8 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    // Removed the 'kotlin-compose' alias here if it's causing issues, 
-    // we will stick to standard XML Views for now as per your layout.
 }
 
 android {
@@ -17,20 +15,24 @@ android {
         versionName = "1.0"
     }
 
-    // This fixes the warning about 'kotlinOptions'
-    compilerOptions {
-        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_1_8)
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    // Use this classic block instead of compilerOptions
+    kotlinOptions {
+        jvmTarget = "1.8"
     }
 }
 
 dependencies {
-    // Use direct implementation for core libraries to avoid "mutation" errors
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.constraintlayout)
 
-    // CameraX - Use versions directly here to ensure they don't get "mutated" during resolve
+    // CameraX
     val camerax_version = "1.3.4"
     implementation("androidx.camera:camera-core:$camerax_version")
     implementation("androidx.camera:camera-camera2:$camerax_version")
